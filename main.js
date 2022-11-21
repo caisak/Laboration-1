848bb57627769c64b2b7a913d653e5c1a5352560
Caisak <caisa.kohlin@gmail.com>
2022-11-21 15:21:51 +0100

    alert and message

---
+++ main.js
@@ -0,0 +1,4 @@
+
+alert("Sorry guys, because of a family emergency I have not completed this assigment. My deepest apologies to you and myself, and I promise to do better once you have failed me. Here's a nice alert because I'm not redoing the course next year")
+
+prompt("Would you like to send me a message?")
